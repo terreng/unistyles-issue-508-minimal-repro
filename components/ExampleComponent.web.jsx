93f055c35c3d131b7dddec6edd8f0b4986e64f66
Terren <11605395+terreng@users.noreply.github.com>
2025-01-21 17:30:10 -0800

    Update ExampleComponent.web.jsx

--- components/ExampleComponent.web.jsx
+++ components/ExampleComponent.web.jsx
@@ -2,13 +2,13 @@ import { StyleSheet } from 'react-native-unistyles';
 import { InnerComponent } from '@/components/InnerComponent';
 
 export const ExampleComponent = ({}) => {
-  const styles = StyleSheet.create({
-    card: {
-      backgroundColor: "gray"
-    },
-  });
-
   return (
-    <InnerComponent style={{ ...styles.card }}/>
+    <InnerComponent style={[styles.card]}/>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "gray"
+  },
+});
